7131a03f2fc4619fbab1824d0f18ce10ae1c0f06
Suman Singh <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-12-25 00:26:09 +0530

    day one basicofproblemdsa

--- Basic/SumOf-N-Numbers.js
+++ Basic/SumOf-N-Numbers.js
@@ -1,10 +1,10 @@
 const arr = [12, 12, 68, 0, 9, 3, 9];
 
-let sum = 0; // Initialize a variable to hold the cumulative sum
+let sum = 0; // Initialize a variable to hold the  sum
 
 const cumulativeSum = arr.map((value) => {
-    sum += value; // Update the cumulative sum by adding the current value
-    return sum; // Return the cumulative sum for each iteration
+    sum += value; // Update the  sum by adding the current value
+    return sum; // Return the  sum for each iteration
 });
 
 console.log("cumulative sum", cumulativeSum);
