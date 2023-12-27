8141caef82c260f859c81cb6473f1ea639c2d726
sumansingh226 <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-12-27 23:17:56 +0530

    formatting code

--- Basic/SumOf-N-Numbers.js
+++ Basic/SumOf-N-Numbers.js
@@ -3,8 +3,8 @@ const arr = [12, 12, 68, 0, 9, 3, 9];
 let sum = 0; // Initialize a variable to hold the  sum
 
 const cumulativeSum = arr.map((value) => {
-    sum += value; // Update the  sum by adding the current value
-    return sum; // Return the  sum for each iteration
+    sum += value;
+    return sum;
 });
 
 console.log("cumulative sum", cumulativeSum);
