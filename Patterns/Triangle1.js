48d1651679a7baecd082d19600dbfdbe90b2b0e1
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-12-27 21:03:59 +0530

    triangle patterns

---
+++ Patterns/Triangle1.js
@@ -0,0 +1,18 @@
+// make stars pattern something like this  :
+// *
+// **
+// ***
+// ****
+
+const printSquareStarPatter = ((rows) => {
+
+    for (let i = 0; i < rows; i++) {
+        let pattern = '';
+        for (let j = 0; j < i; j++) {
+            pattern += '* ';
+        }
+        console.log(pattern);
+    }
+})
+let i = 5;
+printSquareStarPatter(i)
