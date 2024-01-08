df04ade98db26cb03b21da46528973d6c15dbe45
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-09 00:14:17 +0530

    count character in given string

---
+++ Practies/CountChartInString.js
@@ -0,0 +1,21 @@
+function charCount(str) {
+    let countObject = {};
+
+    if (typeof str !== "string") {
+        return console.log("Given input is not a valid string");
+    }
+
+    let charCountString = str.replace(/[^a-zA-Z]/g, "").toLowerCase();
+
+    for (let i = 0; i < charCountString.length; i++) {
+        let char = charCountString[i];
+        if (countObject[char]) {
+            countObject[char]++;
+        } else {
+            countObject[char] = 1;
+        }
+    }
+    console.log("countObject", countObject);
+}
+
+charCount("Suman   Singh is a software engineer");
