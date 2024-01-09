588ff507ba407ffb3c9d6707ff2887cf420b55d2
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-10 00:01:03 +0530

    check if 1st arry items is present in array2

--- Practies/FrequencyCounter.js
+++ Practies/FrequencyCounter.js
@@ -1,13 +1,34 @@
+const frequencyCount = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+        console.log("Arrays have different lengths. Cannot compare.");
+        return;
+    }
 
+    const squaredMap = {}; // Store the frequency of squares of elements in arr1
 
-const frequencyCount = ((arr1, arr2) => {
+    // Count the frequency of squares of elements in arr1
     for (let i = 0; i < arr1.length; i++) {
-
-        //check if corospondance array has square value
-        if (arr2[i] === arr1[i] * arr1[i]) {
-            console.log("true");
+        const square = arr1[i] * arr1[i];
+        if (squaredMap[square]) {
+            squaredMap[square]++;
+        } else {
+            squaredMap[square] = 1;
         }
     }
-})
-frequencyCount([1, 2, 3, 4], [1, 4, 6, 12])
+    console.log("squaredMap", squaredMap);
+    // Check if elements in arr2 are squares of elements in arr1
+    for (let i = 0; i < arr2.length; i++) {
+        if (squaredMap[arr2[i]]) {
+            squaredMap[arr2[i]]--;
+        } else {
+            console.log("squaredMap", squaredMap);
+            console.log("false");
+            return;
+        }
+    }
+    console.log("squaredMap", squaredMap);
+    console.log("true");
+};
 
+// Test the function
+frequencyCount([1, 2, 3, 4], [1, 4, 5, 16]); // Output: true
