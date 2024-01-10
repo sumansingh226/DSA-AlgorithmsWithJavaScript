488de7fc9466409f2e0d906b3e6a89cf81b1cfe2
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-10 23:10:10 +0530

    multipleline commments

--- FrequencyCounter/Anagram.js
+++ FrequencyCounter/Anagram.js
@@ -1,6 +1,25 @@
-// An anagram is a word or phrase formed by rearranging the letters of another word or phrase,
-//  using all the original letters exactly once. Essentially,
-//   it's a permutation of the letters of a word or phrase to create a new word or phrase.
+/*An anagram is a word or phrase formed by rearranging the letters of another word or phrase,
+using all the original letters exactly once. Essentially,
+ it's a permutation of the letters of a word or phrase to create a new word or phrase.
 
-// For example, the word "listen" and "silent" are anagrams of each other because they contain
-// the exact same letters.
+For example, the word "listen" and "silent" are anagrams of each other because they contain
+the exact same letters.*/
+
+const checkIsGivenStringAnaGram = inputString => {
+
+    const countChars = {};
+
+    for (const value of inputString) {
+        if (countChars[value]) {
+            countChars[value]++;
+        }
+        else {
+            countChars[value] = 1;
+        }
+
+    }
+    console.log(countChars);
+
+}
+
+checkIsGivenStringAnaGram('anagram')
