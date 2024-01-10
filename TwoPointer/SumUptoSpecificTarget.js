33bf345ac63709b785c7b2932474461f925d4c2c
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-11 00:22:26 +0530

    change  function name

--- TwoPointer/SumUptoSpecificTarget.js
+++ TwoPointer/SumUptoSpecificTarget.js
@@ -1,9 +1,8 @@
 // Given a sorted array, find two elements that sum up to a specific target.
 
-function twoSum(arr, target) {
+function twoSumToSpecificGivenTarget(arr, target) {
     let left = 0,
         right = arr.length - 1;
-
     while (left < right) {
         let sum = arr[left] + arr[right];
 
@@ -19,7 +18,6 @@ function twoSum(arr, target) {
     return [];
 }
 
-// 
 const array = [2, 7, 11, 15, 18];
 const targetSum = 26;
-console.log(twoSum(array, targetSum));
+console.log(twoSumToSpecificGivenTarget(array, targetSum));
