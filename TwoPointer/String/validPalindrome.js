c04539259a81215588eea50d1cc80911f0098a02
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-12 23:11:09 +0530

    valid palindrom string

--- TwoPointer/String/validPalindrome.js
+++ TwoPointer/String/validPalindrome.js
@@ -3,3 +3,17 @@
 // Problem 1: Valid Palindrome
 // Problem Statement:
 // Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+function isPalindrome(s) {
+    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let left = 0, right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
