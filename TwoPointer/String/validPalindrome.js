785346b84b46728883e36e76425e70bf9b5f90a9
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-13 23:44:48 +0530

    code refactor

--- TwoPointer/String/validPalindrome.js
+++ TwoPointer/String/validPalindrome.js
@@ -2,7 +2,10 @@
 
 // Problem 1: Valid Palindrome
 // Problem Statement:
-// Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+// Given a
+//  string, determine if it is a palindrome, considering 
+//  only alphanumeric characters and ignoring cases.
+
 function isPalindrome(s) {
     s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     let left = 0, right = s.length - 1;
@@ -17,3 +20,7 @@ function isPalindrome(s) {
 
     return true;
 }
+
+
+
+console.log(isPalindrome(abcd));
