1f1ea63cf0e33357e1158e9271ecf18ac06d640d
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-13 23:59:40 +0530

    frequency

---
+++ FrequencyCounter/ArrayUniqueValue.js
@@ -0,0 +1,22 @@
+function countFrequency(arr) {
+    // Create an empty object to store frequencies
+    const frequencyCounter = {};
+
+    // Loop through the array
+    for (let element of arr) {
+        // If the element is already in the counter, increment its count
+        if (frequencyCounter[element]) {
+            frequencyCounter[element]++;
+        } else {
+            // If the element is not in the counter, add it with a count of 1
+            frequencyCounter[element] = 1;
+        }
+    }
+
+    // Return the frequency counter object
+    return frequencyCounter;
+}
+
+// Example usage:
+const arr = [1, 2, 3, 4, 1, 2, 2, 3, 4, 5, 5];
+console.log(countFrequency(arr));
