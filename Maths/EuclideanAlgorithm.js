1c10a4efb4657945f94f0e4e8472122ff4b2e17e
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-16 11:23:10 +0530

    Euclidean Algorithm

---
+++ Maths/EuclideanAlgorithm.js
@@ -0,0 +1,7 @@
+/*Euclidean Algorithm:
+
+Take two numbers, say a and b.
+Divide the larger number by the smaller one.
+Replace the larger number with the smaller one and the smaller number with the remainder.
+Repeat the process until the remainder is zero.
+The last non-zero remainder is the GCD*/
