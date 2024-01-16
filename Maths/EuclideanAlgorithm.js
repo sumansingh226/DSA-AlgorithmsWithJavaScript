48ef109ba097e8f9be299d3444db5609362ef698
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-16 12:32:48 +0530

    Euclidean Algorithm

--- Maths/EuclideanAlgorithm.js
+++ Maths/EuclideanAlgorithm.js
@@ -8,18 +8,18 @@ The last non-zero remainder is the GCD*/
 
 // gcd(n1,n2)=  gcd(n1-n2,n2)
 
-const GcdByEuclidean = (n1, n2) => {
-    while (b !== 0) {
-        const temp = b;
-        b = a % b;
-        a = temp;
+const GcdByEuclidean = (a, b) => {
+    while (a > 0 && b > 0) {
+        if (a > b) a = a % b;
+        else b = b % a;
     }
+    if (a == 0) return b;
     return a;
 };
 
 // Example usage:
-const num1 = 20;
-const num2 = 50;
+const num1 = 11;
+const num2 = 13;
 const result = GcdByEuclidean(num1, num2);
 
 console.log(`The GCD of ${num1} and ${num2} is: ${result}`);
