21fdc95297ba65dfae7393147fc6d9676345953e
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-16 11:22:25 +0530

    gcd

--- Maths/GCD.js
+++ Maths/GCD.js
@@ -9,8 +9,18 @@ Divide the larger number by the smaller one.
 Replace the larger number with the smaller one and the smaller number with the remainder.
 Repeat the process until the remainder is zero.
 The last non-zero remainder is the GCD*/
-const GcdOrHcf = (() => {
 
-})
+function gcd(a, b) {
+    for (let i = Math.min(a, b); i >= 1; i--) {
+        if (a % i === 0 && b % i == 0) {
+            return i;
+        }
+    }
+}
 
-GcdOrHcf(17)
+// Example usage:
+const num1 = 11;
+const num2 = 13;
+const result = gcd(num1, num2);
+
+console.log(`The GCD of ${num1} and ${num2} is: ${result}`);
