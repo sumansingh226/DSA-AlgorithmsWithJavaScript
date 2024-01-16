ea7e5c8cb8846ddfb2baa41abe0e266c07f3d117
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-16 11:08:41 +0530

    gcd

---
+++ Maths/GCD.js
@@ -0,0 +1,9 @@
+/*The greatest common divisor (GCD), also known as the greatest common factor (GCF) or highest common
+ factor (HCF), is the largest positive integer that divides two or more numbers without leaving a remainder.
+  There are various methods to find the GCD of two or more numbers.*/
+
+const GcdOrHcf = (() => {
+
+})
+
+GcdOrHcf(17)
