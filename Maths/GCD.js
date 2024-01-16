599355d628e32ae2bd83d3754d96cba1ccce3591
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-16 11:09:06 +0530

    Euclidean Algorithm

--- Maths/GCD.js
+++ Maths/GCD.js
@@ -2,6 +2,13 @@
  factor (HCF), is the largest positive integer that divides two or more numbers without leaving a remainder.
   There are various methods to find the GCD of two or more numbers.*/
 
+/*Euclidean Algorithm:
+
+Take two numbers, say a and b.
+Divide the larger number by the smaller one.
+Replace the larger number with the smaller one and the smaller number with the remainder.
+Repeat the process until the remainder is zero.
+The last non-zero remainder is the GCD*/
 const GcdOrHcf = (() => {
 
 })
