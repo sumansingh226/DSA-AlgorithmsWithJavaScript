815c11dc74ceea340d7da44045ddd0a9277671f4
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-18 15:55:40 +0530

    reverse string

--- Practies/ReverseStringWords.js
+++ Practies/ReverseStringWords.js
@@ -2,7 +2,6 @@
 const reverseWordsInString = ((str) => {
     let newStr = str.split(' ');
     let result = "";
-
     for (let i = newStr.length - 1; i >= 0; i--) {
         result += `${newStr[i]} `;
     }
