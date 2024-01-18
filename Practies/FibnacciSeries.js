f369f64c2b37890030ae10123e716fe70771c399
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-18 16:38:26 +0530

    fibnaaci series

---
+++ Practies/FibnacciSeries.js
@@ -0,0 +1,12 @@
+function generateFibonacci(n) {
+    let fibSeries = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+        fibSeries[i] = fibSeries[i - 1] + fibSeries[i - 2];
+    }
+
+    return fibSeries;
+}
+
+// Example: Generate the first 10 Fibonacci numbers
+console.log(generateFibonacci(10));
