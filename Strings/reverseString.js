59cee274bcdb3a7337384c74a01a325d3eba7567
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-18 18:30:23 +0530

    time and space complexity

--- Strings/reverseString.js
+++ Strings/reverseString.js
@@ -13,6 +13,15 @@ let originalString = "Hello, World!";
 let reversedString = reverseString(originalString);
 console.log("Original String:", originalString);
 console.log("Reversed String:", reversedString);
+/*
+Time Complexity:
+The time complexity of the reverseString function is O(n), where n is the length of the input string.
+ This is because the function iterates through each character in the input string exactly once, 
+ and the number of iterations is directly proportional to the length of the string.
 
-Time complexity: 
 Space Complexity:
+The space complexity is also O(n). In the optimized approach, we use an array (reversedArray) to store the reversed
+ characters. The size of this array is equal to the length of the input string. Therefore, the space complexity is linear with respect to the size of the input.
+
+Time Complexity: O(n)
+Space Complexity: O(n)*/
