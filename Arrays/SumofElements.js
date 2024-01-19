4c2d663cd19b728ef5379fdfd46320fde70a060e
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-20 00:39:34 +0530

    2. **Calculate the sum of elements in an array.**

--- Arrays/SumofElements.js
+++ Arrays/SumofElements.js
@@ -1,1 +1,17 @@
-2. ** Calculate the sum of elements in an array.**
+// 2. ** Calculate the sum of elements in an array.**
+
+
+const sumOfElemets = ((array) => {
+    let sum = 0;
+    for (const iterator of object) {
+        sum += iterator;
+    }
+
+    return sum;
+
+});
+
+
+const array = [2, 5, -4, 7, 6]
+const result = sumOfElemets(array);
+console.log();
