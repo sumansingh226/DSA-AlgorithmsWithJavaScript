b96a3f25bd93495e45d787f7a5fcb850de9b4f14
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-23 00:19:52 +0530

    is array sorted

---
+++ Arrays/4IsArraySorted.js
@@ -0,0 +1,14 @@
+function isSorted(arr) {
+    const n = arr.length;
+
+
+
+
+}
+
+// Example usage:
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 2, 8, 1, 3];
+
+console.log(isSorted(arr1)); // Output: true
+console.log(isSorted(arr2)); // Output: false
