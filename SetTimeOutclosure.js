45ca901d2f424cecdefbeaa407432f321203b546
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-23 18:23:40 +0530

    closure

---
+++ SetTimeOutclosure.js
@@ -0,0 +1,24 @@
+// function x() {
+//     for (let i = 1; i <= 5; i++) {
+//         setTimeout(function () {
+//             console.log(i);
+//         }, i * 1000);
+//     }
+
+//     console.log("Namaste  JS");
+// }
+// x();
+
+function x() {
+    for (var i = 1; i <= 5; i++) {
+        function close(x) {
+            setTimeout(function () {
+                console.log(x);
+            }, x * 1000);
+        }
+        close(i);
+    }
+
+    console.log("Namaste  JS");
+}
+x();
