3d5ac931b30b0f6f1b93d9ec54f91cf4bcf78d34
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-25 18:49:07 +0530

    is primse

---
+++ Maths/isPrimse.js
@@ -0,0 +1,21 @@
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Example: Check if a number is prime
+const yourNumber = 17;
+if (isPrime(yourNumber)) {
+    console.log(`${yourNumber} is a prime number.`);
+} else {
+    console.log(`${yourNumber} is not a prime number.`);
+}
