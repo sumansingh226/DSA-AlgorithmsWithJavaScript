c8d80fdaf1fa63ede2d9e3cced70124035173409
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-25 18:52:11 +0530

    is primse

--- Maths/isPrimse.js
+++ Maths/isPrimse.js
@@ -6,21 +6,18 @@ natural numbers. In other words, a prime number is a positive integer greater th
 
 function isPrime(number) {
     if (number <= 1) {
-        return false;
+        console.log(`${yourNumber} is not a prime number.`);
     }
     for (let i = 2; i <= Math.sqrt(number); i++) {
         if (number % i === 0) {
-            return false;
+            console.log(`${yourNumber} is not a prime number.`);
         }
     }
 
-    return true;
+    console.log(`${yourNumber} is a prime number.`);
 }
 
 // Example: Check if a number is prime
-const yourNumber = 17;
-if (isPrime(yourNumber)) {
-    console.log(`${yourNumber} is a prime number.`);
-} else {
-    console.log(`${yourNumber} is not a prime number.`);
-}
+const n = 17;
+isPrime(n)
+
