a6a990f75f3af1bae41fc5cc413dae4878d92a8a
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-25 18:49:31 +0530

    is primse

--- Maths/isPrimse.js
+++ Maths/isPrimse.js
@@ -2,7 +2,6 @@ function isPrime(number) {
     if (number <= 1) {
         return false;
     }
-
     for (let i = 2; i <= Math.sqrt(number); i++) {
         if (number % i === 0) {
             return false;
