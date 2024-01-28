5ba98d217b6998db3fa379771441d99e2e2f5f42
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-28 23:33:58 +0530

    count elements frequency in array

--- Arrays/7CounteElemetFrequescy.js
+++ Arrays/7CounteElemetFrequescy.js
@@ -5,16 +5,14 @@ const countElelemtyFrequency = (arr) => {
     let frequency = {};
 
     for (let i = 0; i < arr.length; i++) {
-
         frequency[arr[i]] ?
             frequency[arr[i]] += 1
             :
             frequency[arr[i]] = 1
-
     }
     console.log("frequency", frequency);
 
 }
 
 const inputArray = [1, 2, 1, 0, 1, 2, 1];
-const result = countElelemtyFrequency(inputArray);
+countElelemtyFrequency(inputArray);
