630bff147ee99fa0c0613949ef1e4f3b7bba7240
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-28 23:19:49 +0530

    remove duplicate

--- Arrays/6removeDuplicate.js
+++ Arrays/6removeDuplicate.js
@@ -1,1 +1,20 @@
 // 6. **Remove duplicates from an array.**
+
+function removeDuplicates(arr) {
+    const uniqueArray = [];
+    const seenElements = {};
+
+    for (const element of arr) {
+        if (!seenElements[element]) {
+            uniqueArray.push(element);
+            seenElements[element] = true;
+        }
+    }
+
+    return uniqueArray;
+}
+
+// Example usage:
+const array = [1, 2, 3, 4, 2, 5, 6, 1];
+const uniqueArray = removeDuplicates(array);
+console.log("Array without duplicates:", uniqueArray);
