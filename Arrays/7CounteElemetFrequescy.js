0ffc8fcff52a093b4b3dd6dc0aa13a8f20923390
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-28 23:26:58 +0530

    count elements frequency in array

--- Arrays/7CounteElemetFrequescy.js
+++ Arrays/7CounteElemetFrequescy.js
@@ -1,1 +1,15 @@
 // 7. **Count the occurrences of a specific element in an array.**
+
+
+const countElelemtyFrequency = (arr) => {
+    let frequency = {};
+
+    for (let i = 0; i < arr.length; i++) {
+
+    }
+
+
+}
+
+const inputArray = [6, 1, 0, 2, 5, 3, 0, 4, 1, 8, 9, 4, 2, 56, 4, 2, 6, 7, 9, 8, 9, 8, 3];
+const result = countElelemtyFrequency(inputArray);
