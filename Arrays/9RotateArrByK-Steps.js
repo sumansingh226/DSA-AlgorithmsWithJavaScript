77a4ad968ea281ba8f5cc83cecdd0f36841b2c7e
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-29 19:15:50 +0530

    rotate array by k step to right

---
+++ Arrays/9RotateArrByK-Steps.js
@@ -0,0 +1,9 @@
+// Rotate an array to the right by k steps.
+// input nums = [1,2,3,4,5,6,7];
+// k=3
+// step 1 : if k =1 nums = [7,1,2,3,4,5,6]
+// step 2 : if k =2 nums = [7,6,1,2,3,4,5]
+// step 3 : if k =3 nums = [7,6,5,2,3,4,]
+
+
+
