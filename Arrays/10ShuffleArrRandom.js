173d0e8e1c5ed0c63ea65956adba77004600110d
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-30 00:34:18 +0530

    10. **Implement a function to shuffle an array randomly.**

--- Arrays/10ShuffleArrRandom.js
+++ Arrays/10ShuffleArrRandom.js
@@ -4,7 +4,14 @@ function shuffleArray(arr) {
     // Create a copy to avoid modifying the original array
     const shuffledArray = arr.slice();
     const n = shuffledArray.length;
+    for (let i = n - 1; i > 0; i--) {
+        // Generate a random index between 0 and i (inclusive)
+        const j = Math.floor(Math.random() * (i + 1));
 
+
+    }
+
+    return shuffledArray;
 
 }
 
