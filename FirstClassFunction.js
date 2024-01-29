a02badafbcce18c56c22f36169eceab333558b94
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-29 19:10:17 +0530

    First class functions

---
+++ FirstClassFunction.js
@@ -0,0 +1,25 @@
+
+// Function Statement   aka function declaration
+
+function funStateMent() {
+    console.log("this is function statement");
+}
+
+// Function Expression
+var fun = function () {
+
+}
+
+    //anonymous declaration
+    (function () {
+
+    })
+
+
+// named Function Expression
+var fun1 = function cc() {
+
+}
+
+
+
