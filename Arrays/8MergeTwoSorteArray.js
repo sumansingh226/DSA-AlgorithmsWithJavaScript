9ba9b25a0a82d6addb0af1654d9d73df203f1108
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-29 11:40:55 +0530

    Merge two sorted arrays

--- Arrays/8MergeTwoSorteArray.js
+++ Arrays/8MergeTwoSorteArray.js
@@ -33,5 +33,5 @@ function mergeSortedArrays(arr1, arr2) {
 // Example usage:
 const arr1 = [1, 3, 5, 7];
 const arr2 = [2, 4, 6, 8];
-console.log(mergeSortedArrays(arr1, arr2)); // Output: [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(mergeSortedArrays(arr1, arr2));
 
