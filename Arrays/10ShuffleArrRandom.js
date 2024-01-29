8a19c37554fe94f84a3a0201d89f44533f595b5d
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-30 00:33:47 +0530

    10. **Implement a function to shuffle an array randomly.**

--- Arrays/10ShuffleArrRandom.js
+++ Arrays/10ShuffleArrRandom.js
@@ -1,1 +1,10 @@
 // 10. **Implement a function to shuffle an array randomly.**
+
+function shuffleArray(arr) {
+
+}
+
+// Example usage:
+const originalArray = [1, 2, 3, 4, 5];
+const shuffledResult = shuffleArray(originalArray);
+console.log(shuffledResult);
