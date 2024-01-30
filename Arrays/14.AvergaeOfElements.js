8dd09f56c138b19f2197f078b26e5332091d6b72
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-01-30 17:10:35 +0530

    average of array elements

--- Arrays/14.AvergaeOfElements.js
+++ Arrays/14.AvergaeOfElements.js
@@ -5,16 +5,17 @@ const averageOfElements = ((arr) => {
     let avg = 0;
     let n = arr.length;
     let sum = 0;
-    for (let i = 0; i < err.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
 
         sum += arr[i]
     }
+
     avg = sum / n;
     return avg;
 
 });
 
 
-const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const result = linerSearchInArray(inputArray);
+const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const result = averageOfElements(inputArray);
 console.log(result);
