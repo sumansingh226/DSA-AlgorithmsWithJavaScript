7cd42ef2179ddec2c53d6cacc0964fe2ae7cb8af
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-30 23:31:03 +0530

    pair two sum

--- TwoPointer/Arrays/PairTwoSome.js
+++ TwoPointer/Arrays/PairTwoSome.js
@@ -1,4 +1,6 @@
 
+/*Pair Sum in Array: Given an array and a target sum,
+ find if there are two elements in the array that add up to the target sum.*/
 
 function hasPairWithSum(arr, target) {
     let left = 0, right = arr.length - 1;
