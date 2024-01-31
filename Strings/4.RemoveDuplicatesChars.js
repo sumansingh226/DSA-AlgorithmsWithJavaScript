0facb7de3c25e32f695deee8f7c81366e13a52e7
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-01 00:27:23 +0530

    remove duplicates

--- Strings/4.RemoveDuplicatesChars.js
+++ Strings/4.RemoveDuplicatesChars.js
@@ -2,6 +2,26 @@
 
 const removeDuplicatChars = str => {
 
+    let charFreqency = {};
+    let removeDulpcate = "";
+
+    for (const char of str) {
+        if (charFreqency[char]) charFreqency[char] += 1;
+        else charFreqency[char] = 1;
+    }
+    for (const char of str) {
+        if (charFreqency[char] > 1) {
+            charFreqency[char] -= 1;
+        }
+
+    }
+    for (const char of str) {
+        if (charFreqency[char] === 1) {
+            removeDulpcate += char;
+        }
+
+    }
+    return removeDulpcate;
 }
 
 
