b78832e06fe7ba1f85565082a6e9ef64d5bd0da1
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-31 23:54:55 +0530

    first non-repeact chars

--- Strings/3.FirstNonReptChar.js
+++ Strings/3.FirstNonReptChar.js
@@ -1,1 +1,27 @@
 // 3.[] Create a program to find the first non - repeated character in a string.
+
+
+const findFirstNonReptCharInString = str => {
+
+    let frequnecyOfChars = {};
+
+    for (const value of str) {
+        if (frequnecyOfChars[value]) {
+            frequnecyOfChars[value] += 1;
+        }
+        else {
+            frequnecyOfChars[value] = 1;
+
+        }
+    }
+    for (const value of str) {
+        if (frequnecyOfChars[value] === 1) {
+            return frequnecyOfChars[value]
+        }
+
+    }
+    console.log("frequnecyOfChars", frequnecyOfChars);
+}
+
+let input = "suman singh";
+const result = findFirstNonReptCharInString(input);
