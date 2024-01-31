e91099122f591ea9e1006425d8a5a9492d0578bf
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-01 00:20:51 +0530

    remove duplicates

---
+++ Strings/4.RemoveDuplicatesChars.js
@@ -0,0 +1,10 @@
+// 4. [ *] Write a function to remove duplicate characters from a string.
+
+const removeDuplicatChars = str => {
+
+}
+
+
+const str = "abcdabcd";
+const result = removeDuplicatChars(str);
+console.log(result);
