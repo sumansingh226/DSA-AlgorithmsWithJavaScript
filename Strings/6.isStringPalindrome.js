6237efc1d68bed78ca3d029a9fcabb0383797605
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-01 23:13:47 +0530

    check if a string is palindrome

--- Strings/6.isStringPalindrome.js
+++ Strings/6.isStringPalindrome.js
@@ -1,1 +1,11 @@
 // 6. [*] Create a program to check if a string is a palindrome.
+
+
+const idStringPalindrome = str => {
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+
+}
+
+const inputStr = "A man, a plan, a canal, Panama!"
+idStringPalindrome(inputStr);
