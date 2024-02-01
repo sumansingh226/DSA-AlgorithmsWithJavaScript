34fb9a608961dd7170a74afe8a9a150e18793c5d
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-01 23:08:41 +0530

    count no of vowles in string

--- Strings/5.countVowels.js
+++ Strings/5.countVowels.js
@@ -1,24 +1,21 @@
 // 5. [*] Implement a function to count the number of vowels in a string.
+// vowels :  a , e , i ,o ,u
 
-// vowels :  a , e , i ,o ,u 
-
-
-const countNumberOfVowles = str => {
-    const vowlesInAlphabets = ['a', 'e', 'i', 'o', 'u'];
+const countNumberOfVowles = (str) => {
+    const vowlesInAlphabets = ["a", "e", "i", "o", "u"];
     let vowlesInString = [];
 
     for (const char of str) {
         if (vowlesInAlphabets.includes(char) && !vowlesInString.includes(char)) {
-            vowlesInString.push(char)
+            vowlesInString.push(char);
         }
-
     }
     return vowlesInString;
-}
+};
 
-const inputStr = "My Name is Suman Singh. "
+const inputStr = "My Name is Suman Singh. ";
 
-const result = countNumberOfVowles(inputStr)
+const result = countNumberOfVowles(inputStr);
 console.log("vowles presnt in string :", result);
 
 console.log("number vowles presnt in string :", result.length);
