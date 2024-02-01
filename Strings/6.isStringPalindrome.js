cb126703861fd5b4ba51f9d07dbebc3ccb4de640
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-01 23:17:06 +0530

    check if a string is palindrome

--- Strings/6.isStringPalindrome.js
+++ Strings/6.isStringPalindrome.js
@@ -12,3 +12,8 @@ const idStringPalindrome = str => {
 
 const inputStr = "A man, a plan, a canal, Panama!"
 const result = idStringPalindrome(inputStr);
+if (inputStr === result) {
+    console.log(`${inputStr} is a palindrome.`);
+} else {
+    console.log(`${inputStr} is not a palindrome.`);
+}
