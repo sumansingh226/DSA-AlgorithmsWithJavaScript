48a18825584b9db637cef818fca80e923d7563af
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-01 23:22:04 +0530

    check if a string is palindrome

--- Strings/6.isStringPalindrome.js
+++ Strings/6.isStringPalindrome.js
@@ -2,15 +2,14 @@
 
 
 const idStringPalindrome = str => {
-    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     let reverseStr = "";
-    for (let index = cleanStr.length - 1; index > 0; index--) {
-        reverseStr += cleanStr[index];
+    for (let index = str.length - 1; index >= 0; index--) {
+        reverseStr += str[index];
     }
     return reverseStr;
 }
 
-const inputStr = "A man, a plan, a canal, Panama!"
+const inputStr = "racecar"
 const result = idStringPalindrome(inputStr);
 if (inputStr === result) {
     console.log(`${inputStr} is a palindrome.`);
