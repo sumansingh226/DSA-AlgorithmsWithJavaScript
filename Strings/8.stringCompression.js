b011870e4c8221f0284045c1f945bf3e90eaaab1
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-02 00:46:52 +0530

    replace all strimngs with %20

--- Strings/8.stringCompression.js
+++ Strings/8.stringCompression.js
@@ -1,1 +1,20 @@
 // 8. [ ] Implement a function to perform basic string compression using the counts of repeated characters.
+
+
+const basicStringCompression = str => {
+    let countCharFrequency = {};
+    let compressedStr = "";
+
+    for (const value of str) {
+        countCharFrequency[value] === value ? countCharFrequency[value] += 1 : countCharFrequency[value] = 1;
+    }
+    for (const key in countCharFrequency) {
+        console.log(key);
+    }
+
+}
+
+
+const inputString = "aabcccccaaa";
+const result = basicStringCompression(inputString)
+console.log(result);
