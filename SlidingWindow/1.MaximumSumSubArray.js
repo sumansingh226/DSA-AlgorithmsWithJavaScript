e40f374ebdab773c7827058f174f0d0bbda6434c
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-02-02 19:10:40 +0530

    Maximum Sum Subarray of Size K

--- SlidingWindow/1.MaximumSumSubArray.js
+++ SlidingWindow/1.MaximumSumSubArray.js
@@ -4,9 +4,8 @@ function maxSumSubarrayOfSizeK(arr, k) {
     let maxSum = 0;
     let windowSum = 0;
     let windowStart = 0;
-    let windowEnd = 0
 
-    for (windowEnd; windowEnd < arr.length; windowEnd++) {
+    for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
         windowSum += arr[windowEnd];
 
         // Slide the window if we have reached the size K
@@ -22,5 +21,5 @@ function maxSumSubarrayOfSizeK(arr, k) {
 
 // Example usage:
 const arr = [4, 2, 1, 7, 8, 1, 2, 8, 1, 0];
-const k = 5;
+const k = 3;
 console.log("Maximum sum of subarray of size K:", maxSumSubarrayOfSizeK(arr, k)); // Output: 16
