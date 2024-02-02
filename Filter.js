c33dbf26b2a91a86c58e8dedcb59014be3a265ed
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-02-02 13:11:37 +0530

    filter  function

--- Filter.js
+++ Filter.js
@@ -1,5 +1,8 @@
 
 
+// The filter function is used to create a new array containing only the elements of the original array that
+//  satisfy a given condition.
+
 const arr = [-1, 2, -6, 0, 3, 4, 5, 6];
 
 function double(x) {
