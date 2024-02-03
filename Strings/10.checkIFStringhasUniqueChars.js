27ca633c2d9dcc6d6e36d1074ed2c0f307569b42
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-03 23:10:14 +0530

    if strinh has all unique chars

--- Strings/10.checkIFStringhasUniqueChars.js
+++ Strings/10.checkIFStringhasUniqueChars.js
@@ -1,6 +1,9 @@
 // 10. [*] Write a function to check if a string has all unique characters.
 
-const checkIfStrinhHasAllUniqueChars = (str) => { };
+const checkIfStrinhHasAllUniqueChars = (str) => {
+
+    // check if string is non-empty
+};
 
 const inputString = "suman ";
 
