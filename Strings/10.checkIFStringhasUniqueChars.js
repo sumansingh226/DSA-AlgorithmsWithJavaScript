e5422ac04bd221d4495602bbc7d253b8ecace7a7
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-03 23:12:56 +0530

    if strinh has all unique chars

--- Strings/10.checkIFStringhasUniqueChars.js
+++ Strings/10.checkIFStringhasUniqueChars.js
@@ -6,6 +6,13 @@ const checkIfStrinhHasAllUniqueChars = (str) => {
     if (str.length <= 0) {
         return "string must be contain some characters";
     }
+    const countCharFrequency = {};
+
+    for (let index = 0; index < array.length; index++) {
+
+        if (countCharFrequency[str[i]]) countCharFrequency[str[i]] += 1; else
+            countCharFrequency[str[i]] = 1;
+    }
 };
 
 const inputString = "suman ";
