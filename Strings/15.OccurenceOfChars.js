c5113011d135ca16201b8714a960214e4f8bb061
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-05 23:59:18 +0530

    reverse words in string

--- Strings/15.OccurenceOfChars.js
+++ Strings/15.OccurenceOfChars.js
@@ -5,11 +5,11 @@ const occurenceOfChar = str => {
     const charOccurence = {};
     for (const value of str) {
 
-        if (charOccurence[value]) {
-            charOccurence[value] += 1;
+        if (charOccurence[str[value]]) {
+            charOccurence[str[value]] += 1;
         }
         else {
-            charOccurence[value] += 1;
+            charOccurence[str[value]] += 1;
         }
     }
     return charOccurence;
