594944c1a9145e63d20bb227c5d10ab47ddedef9
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-05 23:40:58 +0530

    reverse words in string

--- Strings/11.ReverseWordsInstring.js
+++ Strings/11.ReverseWordsInstring.js
@@ -3,7 +3,11 @@
 
 const revreseWordsInString = str => {
 
-
+    // string , it must have somewords , it should not be empty 
+    if (str.length <= 0) {
+        return "String mshould be contain some words to return"
+    }
 }
 
 const inputString = "";
+revreseWordsInString(inputString)
