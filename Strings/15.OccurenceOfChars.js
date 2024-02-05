fc42ffce5bec67d61d7666f73aeeff4205b3fc3b
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-05 23:56:28 +0530

    reverse words in string

--- Strings/15.OccurenceOfChars.js
+++ Strings/15.OccurenceOfChars.js
@@ -1,1 +1,10 @@
 // 15. [ ] Create a program to count the number of occurrences of a character in a string.
+
+
+const occurenceOfChar = str => {
+
+
+}
+
+const inputString = "SumanSingh"
+occurenceOfChar(inputString);
