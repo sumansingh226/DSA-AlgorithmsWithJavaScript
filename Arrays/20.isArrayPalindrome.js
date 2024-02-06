5845c8f4c2b241e1ec2539ce53d12209fd6ac2bd
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-06 23:46:32 +0530

    is array palindrom

--- Arrays/20.isArrayPalindrome.js
+++ Arrays/20.isArrayPalindrome.js
@@ -3,4 +3,14 @@
 //palidrome means if we revrese and array it should be same 
 
 
-const check
+const checkIsArrayPalidndrome = arr => {
+
+    //  array should not be empty 
+    //if revrese the  elemnts it should be same 
+    let givenArray = arr;
+
+
+}
+
+const inputArray = [4, 5, 5, 4];
+checkIsArrayPalidndrome(inputArray)
