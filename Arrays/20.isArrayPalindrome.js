3417d3a12412b30265818860c6b993d375bce335
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-07 00:03:58 +0530

    is array palindrom

--- Arrays/20.isArrayPalindrome.js
+++ Arrays/20.isArrayPalindrome.js
@@ -3,17 +3,23 @@
 //palidrome means if we revrese and array it should be same 
 
 const checkIsArrayPalindrome = arr => {
-    // Loop through the array from start to end and from end to start simultaneously
-    for (let i = 0, j = arr.length - 1; i < j; i++, j--) {
-        // Compare elements at the current positions
+
+    let i = 0;
+    let j = arr.length - 1;
+
+    while (i < j) {
         if (arr[i] !== arr[j]) {
-            return 'given array is not palindrome';
+            console.log('given array is not palindrome');
+
         }
+        i++;
+        j--;
+
     }
 
-    return 'given array is palindrome';
+    console.log('given array is palindrome');
 }
 
-const inputArray = [1, 2, 3, 2, 1, 5];
+const inputArray = [1, 2, 3, 2, 1, 6];
 const result = checkIsArrayPalindrome(inputArray);
-console.log(result); // Output: given array is palindrome
+console.log(result); 
