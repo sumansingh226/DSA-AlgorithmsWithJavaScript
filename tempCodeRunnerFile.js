ceb57a36bbbe8138f611687ab32ed27a943abe6d
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-02-06 12:25:15 +0530

    call apply bind

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,7 @@
-const calculateCircumference = function (radius) {
+name2 = {
+//     firstName: "suman",
+//     lastName: "chauhan2",
+// }
+
+// //function borrowing
+// name.printFullName.call(name2)
