2f348b75aac8aa3806f4bb22c359dea69748e0ab
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2024-02-07 20:00:32 +0530

    recrusion

--- Recrusion/printNamesNTime.js
+++ Recrusion/printNamesNTime.js
@@ -1,26 +1,42 @@
 
-//print Name n time 
-function printNameNTime(n, i) {
-    if (i > n) {
+// //print Name n time 
+// function printNameNTime(n, i) {
+//     if (i > n) {
+//         return;
+//     } else {
+//         console.log("This is Suman Singh", i);
+//         printNameNTime(n, i + 1);
+//     }
+// }
+
+// printNameNTime(10, 1);
+
+// //print  1 to n integer  time 
+
+
+// function printN1toN(n, i) {
+//     if (i > n) {
+//         return;
+//     } else {
+//         console.log(i);
+//         printN1toN(n, i + 1);
+//     }
+// }
+
+// printN1toN(10, 1);
+
+
+
+//print  n to  1integer  time 
+
+
+function printNto1(n) {
+    if (n <= 0) {
         return;
     } else {
-        console.log("This is Suman Singh", i);
-        printNameNTime(n, i + 1);
+        console.log(n);
+        printNto1(n - 1);
     }
 }
 
-printNameNTime(10, 1);
-
-//print  n integer  time 
-
-
-function printNameTime(n, i) {
-    if (i > n) {
-        return;
-    } else {
-        console.log(i);
-        printNameTime(n, i + 1);
-    }
-}
-
-printNameTime(10, 1);
+printNto1(10);
