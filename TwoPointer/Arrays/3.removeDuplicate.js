a4e83739d23c1cfda6c4429f93bcc089bf94f06d
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-09 00:20:58 +0530

    remove duplicates

--- TwoPointer/Arrays/3.removeDuplicate.js
+++ TwoPointer/Arrays/3.removeDuplicate.js
@@ -24,8 +24,7 @@ function removeDuplicates(nums) {
     return uniqueIndex + 1;
 }
 
-// Example usage:
 let nums = [0, 0, 1, 1, 2, 2, 3, 3, 4];
 let length = removeDuplicates(nums);
-console.log("New length:", length); // Output: 5
-console.log("Array after removing duplicates:", nums.slice(0, length)); // Output: [0, 1, 2, 3, 4]
+console.log("New length:", length);
+console.log("Array after removing duplicates:", nums.slice(0, length)); 
