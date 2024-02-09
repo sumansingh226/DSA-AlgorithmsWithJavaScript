49e339518884b27147cb5e7be5148b158a8c6067
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-10 00:23:12 +0530

    sort the array

---
+++ Practies/sortArray.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+let array = [5, 2, 8, 1, 4];
+console.log(bubbleSort(array)); // Output: [1, 2, 4, 5, 8]
