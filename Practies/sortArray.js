411d75082112502c440a68e1f59b0926dcefcad0
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-11 00:43:08 +0530

    sort the array

--- Practies/sortArray.js
+++ Practies/sortArray.js
@@ -3,7 +3,6 @@ function bubbleSort(arr) {
     for (let i = 0; i < len; i++) {
         for (let j = 0; j < len - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap elements
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
