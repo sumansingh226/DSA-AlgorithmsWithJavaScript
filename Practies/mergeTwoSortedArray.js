16ba73c1ef3d3bf0444abb2172d04ba83e454687
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-12 18:54:10 +0530

    merge two sorted array

--- Practies/mergeTwoSortedArray.js
+++ Practies/mergeTwoSortedArray.js
@@ -28,7 +28,6 @@ function mergeSortedArrays(arr1, arr2) {
     return mergedArray;
 }
 
-// Example usage:
 const arr1 = [1, 3, 5, 7, 9];
 const arr2 = [2, 4, 6, 8, 10];
 console.log(mergeSortedArrays(arr1, arr2)); 
