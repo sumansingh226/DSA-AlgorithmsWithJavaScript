0d43c77daea0c00de8be4f9429d095e98df59c1e
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-12 18:54:23 +0530

    merge two sorted array

--- Practies/mergeTwoSortedArray.js
+++ Practies/mergeTwoSortedArray.js
@@ -13,13 +13,11 @@ function mergeSortedArrays(arr1, arr2) {
         }
     }
 
-    // If there are remaining elements in arr1, push them to the merged array
     while (pointer1 < arr1.length) {
         mergedArray.push(arr1[pointer1]);
         pointer1++;
     }
 
-    // If there are remaining elements in arr2, push them to the merged array
     while (pointer2 < arr2.length) {
         mergedArray.push(arr2[pointer2]);
         pointer2++;
