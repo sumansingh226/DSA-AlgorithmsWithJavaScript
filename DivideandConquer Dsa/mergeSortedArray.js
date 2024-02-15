3f7a43dffbcab08a4f29def99e97317494cbce7e
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-16 00:39:17 +0530

    merge sorted array

---
+++ DivideandConquer Dsa/mergeSortedArray.js
@@ -0,0 +1,31 @@
+function mergeSortedArrays(arr1, arr2) {
+    let mergedArray = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArray.push(arr1[i]);
+            i++;
+        } else {
+            mergedArray.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        mergedArray.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        mergedArray.push(arr2[j]);
+        j++;
+    }
+
+    return mergedArray;
+}
+
+const arr1 = [1, 3, 5, 7];
+const arr2 = [2, 4, 6, 8];
+console.log(mergeSortedArrays(arr1, arr2)); 
