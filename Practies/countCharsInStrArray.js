c5df87af3685f31c2e5a27f3d7ce97cb22814753
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-26 20:02:14 +0530

    number of chars in array str

--- Practies/countCharsInStrArray.js
+++ Practies/countCharsInStrArray.js
@@ -6,10 +6,13 @@ const countCharsInGivenArray = (arr) => {
     let length = 0;
     for (const value of arr) {
 
-        length += value.length;
+        for (const len of value) {
+            length += 1;
+        }
     }
     return length;
 }
 
 const arr = ["char1", "char2", "char3", "char4"];
 const result = countCharsInGivenArray(arr);
+console.log("result", result);
