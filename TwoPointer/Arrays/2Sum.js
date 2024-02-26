fe02208360c73a49acf175e47874af88f3463e42
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-26 22:08:53 +0530

    update code by two pointer

--- TwoPointer/Arrays/2Sum.js
+++ TwoPointer/Arrays/2Sum.js
@@ -3,15 +3,21 @@
 Problem Statement:
 Given an array of integers, return indices of the two numbers 
 such that they add up to a specific target. */
-
 function twoSum(nums, target) {
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        const sum = nums[left] + nums[right];
+        if (sum === target) {
+            return [left, right];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
         }
     }
+
     return null; // If no solution is found
 }
 
@@ -20,3 +26,4 @@ const nums = [2, 7, 11, 15];
 const target = 9;
 const result = twoSum(nums, target);
 console.log(result); // Output: [0, 1]
+
