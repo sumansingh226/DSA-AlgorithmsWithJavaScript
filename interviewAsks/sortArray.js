ead64fb5564a3d4cbddc42f34a8b22e9f27d91a6
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-01 21:34:45 +0530

    sort arrys in js optimized

--- interviewAsks/sortArray.js
+++ interviewAsks/sortArray.js
@@ -1,24 +1,26 @@
 // sort an  array of integers
 
 
-function sortRaayOfInteger(arr) {
-    const sortedArray = [];
+function sortArrayOfInteger(arr) {
+    const n = arr.length;
+    let swapped;
 
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - 1 - i; j++) {
-
+    for (let i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (let j = 0; j < n - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 const temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-
+                swapped = true;
             }
         }
+        if (!swapped) break; // If no swaps were made, the array is already sorted
     }
 
     return arr;
 }
 
 const arr = [2, 5, 1, 3, 6, 0, 9, 4];
-const result = sortRaayOfInteger(arr);
+const result = sortArrayOfInteger(arr);
 console.log(result);
