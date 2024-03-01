6f220d457a5142a66b1739b2aa7c10d2923cff74
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-01 21:32:21 +0530

    sort arrys in js

--- interviewAsks/sortArray.js
+++ interviewAsks/sortArray.js
@@ -5,11 +5,20 @@ function sortRaayOfInteger(arr) {
     const sortedArray = [];
 
     for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
 
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+            }
+        }
     }
 
-
+    return arr;
 }
 
 const arr = [2, 5, 1, 3, 6, 0, 9, 4];
-const result = sortRaayOfInteger(arr)
+const result = sortRaayOfInteger(arr);
+console.log(result);
