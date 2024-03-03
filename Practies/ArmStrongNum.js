d1c44b1d8755240592e233cc12df2045b6844b37
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-04 00:22:37 +0530

    isArmStrong

--- Practies/ArmStrongNum.js
+++ Practies/ArmStrongNum.js
@@ -1,11 +1,25 @@
 //  if n = 371  then if 3qb +7qb+1qb  =371 then armstrong  nums
 
 function isArmStrong(num) {
+    let sum = 0;
+    let n = num;
+    while (n > 0) {
+        let digit = n % 10;
+        sum += Math.pow(digit, 3);
+        n = Math.floor(n / 10);
+
+    }
+    if (num === sum) {
+        return `${num} is armStrong number`;
+    }
+    else {
+        return `${num} is not armStrong number`;
+    }
 
 }
 
 
-const n = 371;
+const n = 372;
 const result = isArmStrong(n);
-console.log("result");
+console.log(result);
 
