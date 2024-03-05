70e9845e2b5cdaf5ffe34bce9d9bc9b144189c10
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-05 22:01:46 +0530

    find intersection

--- Maths/intersection.js
+++ Maths/intersection.js
@@ -1,9 +1,33 @@
 //   find unique and command array in two gien arraty
 
 
-const intersection = (arr1, arr2) => {
+function intersection(arr1, arr2) {
+    // Sort the arrays (if they are not already sorted)
+    arr1.sort((a, b) => a - b);
+    arr2.sort((a, b) => a - b);
 
+    const result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            i++;
+        } else if (arr1[i] > arr2[j]) {
+            j++;
+        } else {
+            // If the elements are equal, add to result and move both pointers
+            result.push(arr1[i]);
+            i++;
+            j++;
+        }
+    }
+
+    return result;
 }
+
+
+
 const arr1 = [1, 5, 7, 6, 7, 4, 6, 9, 3];
 const arr2 = [5, 4, 0, 88, 3, 6, 2, 4]
 const result = intersection(arr1, arr2);
