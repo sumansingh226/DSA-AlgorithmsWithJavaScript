6492bc46a77a3c29149339147712358b7c57f20c
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-05 21:53:43 +0530

    find intersection

---
+++ Maths/intersection.js
@@ -0,0 +1,10 @@
+//   find unique and command array in two gien arraty
+
+
+const intersection = (arr1, arr2) => {
+
+}
+const arr1 = [1, 5, 7, 6, 7, 4, 6, 9, 3];
+const arr2 = [5, 4, 0, 88, 3, 6, 2, 4]
+const result = intersection(arr1, arr2);
+console.log("result", result)
