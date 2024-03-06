58530e5360ee5ff48b220e184ec129d161cb721b
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-06 22:44:36 +0530

    //union of two arrays in js

--- Maths/unionoftwoarrays.js
+++ Maths/unionoftwoarrays.js
@@ -1,1 +1,33 @@
 //union of two arrays in js
+function mergeArrays(arr1, arr2) {
+    let pointer1 = 0;
+    let pointer2 = 0;
+    const merged = [];
+
+    while (pointer1 < arr1.length && pointer2 < arr2.length) {
+        if (arr1[pointer1] <= arr2[pointer2]) {
+            merged.push(arr1[pointer1]);
+            pointer1++;
+        } else {
+            merged.push(arr2[pointer2]);
+            pointer2++;
+        }
+    }
+
+    while (pointer1 < arr1.length) {
+        merged.push(arr1[pointer1]);
+        pointer1++;
+    }
+
+    while (pointer2 < arr2.length) {
+        merged.push(arr2[pointer2]);
+        pointer2++;
+    }
+
+    return merged;
+}
+
+// Example usage
+const arr1 = [1, 3, 5, 7];
+const arr2 = [2, 4, 6, 8, 3, 3];
+console.log(mergeArrays(arr1, arr2)); // Output: [1, 2, 3, 4, 5, 6, 7, 8]
