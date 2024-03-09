691b85558ae64f0b338968f87ee5479d68534050
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-10 01:39:35 +0530

    liner search

--- searching/linerSearch.js
+++ searching/linerSearch.js
@@ -1,3 +1,8 @@
+// Write a function called linearSearch which accepts an array and a value, 
+// and returns the index at which the value exists. 
+// If the value does not exist in the array, return -1.
+
+
 function linearSearch(arr, val) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === val) {
