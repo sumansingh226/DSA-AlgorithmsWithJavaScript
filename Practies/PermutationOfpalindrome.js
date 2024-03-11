51bd9110ddc4854bbf57c537c7b28d9df82704e7
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-11 23:13:04 +0530

    permutation of palindrome

--- Practies/PermutationOfpalindrome.js
+++ Practies/PermutationOfpalindrome.js
@@ -4,3 +4,25 @@
 //  A permutation is a rearrangement of letters.
 //  The palindrome does not need to be limited to just dictionary words.
 
+function isPermutationOfPalindrome(str) {
+    str = str.replace(/ /g, '').toLowerCase();
+    let charCount = {};
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+    let oddCount = 0;
+
+    for (let count of Object.values(charCount)) {
+        if (count % 2 !== 0) {
+            oddCount++;
+            if (oddCount > 1) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+console.log(isPermutationOfPalindrome("Tact Coa"));
+console.log(isPermutationOfPalindrome("hello")); 
