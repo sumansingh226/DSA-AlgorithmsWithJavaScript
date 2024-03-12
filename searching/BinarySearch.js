e4b22ab0b91466ef697608bfdf3e13a7b0f896e1
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-12 23:22:07 +0530

    binary search

---
+++ searching/BinarySearch.js
@@ -0,0 +1,25 @@
+// Binary search is a technique used to quickly find a value in a sorted array. 
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+const arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21];
+const target = 13;
+const index = binarySearch(arr, target);
+console.log(index);
