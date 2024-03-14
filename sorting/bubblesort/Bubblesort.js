547628bee51aacb8d844cfdc6c3355e06bb876ae
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-14 23:13:38 +0530

    bubble sort

---
+++ sorting/bubblesort/Bubblesort.js
@@ -0,0 +1,5 @@
+// Bubble sort is a simple sorting algorithm that repeatedly steps through the list,
+//  compares adjacent elements, and swaps them if they are
+// in the wrong order. The pass through the list is repeated until the list is sorted.
+//  It's not efficient for large datasets, but it's easy to understand and implement,
+//   making it suitable for educational purposes or small datasets
