8a3e7b8b1d33a21ac255b027e8ffdf4b2c487aaa
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-16 21:45:27 +0530

    selection sort

--- sorting/selectionsort/selectionsort.js
+++ sorting/selectionsort/selectionsort.js
@@ -1,3 +1,35 @@
-// Selection sort is a simple and efficient sorting algorithm that works by repeatedly 
-// selecting the smallest (or largest) element from the unsorted portion of
-//  the list and moving it to the sorted portion of the list. 
+
+/*Selection Sort
+
+Definition: Selection Sort is a simple sorting algorithm that divides the input array 
+into two parts: the sorted subarray and the unsorted subarray. Initially, the sorted subarray 
+is empty, and the unsorted subarray is the entire input array. The algorithm selects the smallest
+ (or largest, depending on the sorting order) element from the unsorted subarray and swaps it with the 
+ leftmost unsorted element, thus expanding the sorted subarray's boundary by one element.
+
+Algorithm:
+
+Repeat until the unsorted subarray becomes empty:
+Find the minimum element in the unsorted subarray.
+Swap the minimum element with the leftmost unsorted element.
+Complexity:
+
+Time Complexity: O(n^2) for all cases (best, average, and worst).
+Space Complexity: O(1) as it requires only a constant amount of additional space.
+Advantages:
+
+Simple implementation.
+In-place sorting algorithm (doesn't require extra space).
+Good for small arrays or when auxiliary memory is limited.
+Disadvantages:
+
+Inefficient for large arrays due to its O(n^2) time complexity.
+Not stable (relative order of equal elements may change).
+Example:
+
+Input Array: [64, 25, 12, 22, 11]
+Step 1: [11, 25, 12, 22, 64] (11 is the smallest, swap with the first element)
+Step 2: [11, 12, 25, 22, 64] (12 is the smallest in the remaining, swap with the second element)
+Step 3: [11, 12, 22, 25, 64] (22 is the smallest in the remaining, swap with the third element)
+Step 4: [11, 12, 22, 25, 64] (25 is the smallest in the remaining, swap with the fourth element)
+Step 5: [11, 12, 22, 25, 64] (64 is already in the correct position)*/
