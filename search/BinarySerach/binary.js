e80d13c503cdfa81e164295f27ebe753612767a8
sumansingh226 <sumansingh13062000@gmail.com>
2024-04-07 23:21:24 +0530

    binary search1

---
+++ search/BinarySerach/binary.js
@@ -0,0 +1,20 @@
+function binarySearchRecursive(arr, target, left = 0, right = arr.length - 1) {
+    if (left > right) {
+        return -1; // Not found
+    }
+
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+        return mid;
+    } else if (arr[mid] < target) {
+        return binarySearchRecursive(arr, target, mid + 1, right);
+    } else {
+        return binarySearchRecursive(arr, target, left, mid - 1);
+    }
+}
+
+let arr = [1, 3, 5, 7, 9, 11, 13, 15];
+let target = 7;
+let index = binarySearchRecursive(arr, target);
+console.log(index); 
