dc0bc9f9a7af4b64b92aacead78e5b5d4a50a09c
Suman singh <sumansingh13062000@gmail.com>
2025-01-01 21:17:18 +0530

    two  sorted array

--- Practies/TwoSoredArray.js
+++ Practies/TwoSoredArray.js
@@ -1,12 +1,46 @@
 // mere two sorted array
 
+// const mergeSortedArrays = (arr1, arr2) => {
+//     let mergedArray = [];
+//     let i = 0;
+//     let j = 0;
 
+//     while (i < arr1.length && j < arr2.length) {
+//         if (arr1[i] < arr2[j]) {
+//             mergedArray.push(arr1[i]);
+//             i++;
+//         } else {
+//             mergedArray.push(arr2[j]);
+//             j++;
+//         }
+//     }
+//     while (i < arr1.length) {
+//         mergedArray.push(arr1[i]);
+//         i++;
+//     }
+//     while (j < arr2.length) {
+//         mergedArray.push(arr2[j]);
+//         j++;
+//     }
+
+//     return mergedArray;
+// }
 
 const mergingTwoSortedArray = (sortedArray1, sortedArray2) => {
 
     let mergedArray = [];
     let i = 0;
     let j = 0;
-
+    while (i < sortedArray1.length && j < sortedArray2.length) {
+        if (sortedArray1[i] < sortedArray1[j]) {
+            mergedArray.push(sortedArray1[i])
+            i++;
+        }
+        else
+        {
+            mergedArray.push(sortedArray2[j]);
+            j++;
+        }
+    }
 
 }
